90e6565d0bb7d915ef1a22fd4aa26ff66c16c514
statsnguts <157211975+statsnguts@users.noreply.github.com>
2026-02-25 21:13:03 +0530

    adding more details

---
+++ UsingVisualStudio/Chapter2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chapter2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is test");
+        }
+    }
+}
